cc21d0474ff3d41b3868a87586c72dc894c8d044
Umar <olowonyoumar@gmail.com>
2026-09-11 22:06:23 +0100

    Added middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path, Query
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from enum import Enum
 from backend.students.routes import studentRouter
@@ -12,7 +13,16 @@ app = FastAPI()
 app.include_router(studentRouter)
 
 
+origins = [
+    "http://localhost:5173/"
+]
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #Create tables in the database if they don't exist
 Base.metadata.create_all(bind=engine)
